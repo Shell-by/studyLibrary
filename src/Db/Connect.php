655c9bad9d81c18b9f0d84f30f6af605bb1e7f55
Shell-by <wnsghks1026@naver.com>
2023-05-11 15:22:31 +0900

    first

---
+++ src/Db/Connect.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Shellby\Db;
+
+use Exception;
+use PDO;
+
+class Connect {
+
+  private PDO $pdo;
+  
+  public function __construct(
+    string $hostname,
+    string $dbname,
+    string $username,
+    string $password,
+    string $database = "mysql",
+    int $port = 3306,
+    string $charset = "utf8",
+    array $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION),
+  ) {
+    $dsn = "{$database}:host={$hostname};port={$port};dbname={$dbname};charset={$charset}";
+    try {
+      $this->pdo = new PDO($dsn, $username, $password, $option);
+    } catch(Exception $e) {
+      print($e);
+    }
+  } 
+
+  public static function selectAll(
+    string $table,
+  ): array {
+    $stmt = self::$pdo->prepare("SELECT :column FROM :table");
+    $stmt->bindParam(":column", "*");
+    $stmt->bindParam(":table", $table);
+    $stmt->execute();
+    
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+  
+  public static function select(
+    string $column,
+    string $table,
+    ): array {
+      $stmt = self::$pdo->prepare("SELECT :column FROM :table");
+      $stmt->bindParam(":column", $column);
+      $stmt->bindParam(":table", $table);
+      $stmt->execute();
+
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+
+  public static function update(
+    string $column,
+    string $table,
+    string $where = "1"
+  ):int {
+    $stmt = self::$pdo->prepare("UPDATE :table SET :column WHERE :where");
+    $stmt->bindParam(":table", $table);
+    $stmt->bindParam(":column", $column);
+    $stmt->bindParam(":where", $where);
+    return 0;
+  }
+
+}
